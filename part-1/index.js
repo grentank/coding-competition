d90034ce0340b65eddf9ea949280fe9748419865
zmkt <zmkt@mail.ru>
2024-06-21 18:26:35 +0300

    done

--- part-1/index.js
+++ part-1/index.js
@@ -1,14 +1,13 @@
 function createCounter (){
-  let counter = 0
-  return {
-    increment: function() {
-      counter += 1
-    },
-    currentValue: function() {
-      return counter
-    }
-  }
+	let counter = 0;
+	return {
+	increment: function() {
+		counter += 1
+	},
+	currentValue: function() {
+		return counter
+	}
+	};
 }
-console.log(createCounter(0))
 
 module.exports = createCounter;
