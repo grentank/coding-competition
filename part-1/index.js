c8bafed94ac9ba5aa905ef48e80ce87f9275d747
artyomkulik <kulikartem2002@gmail.com>
2024-06-21 18:15:04 +0300

    done

--- part-1/index.js
+++ part-1/index.js
@@ -1,5 +1,11 @@
 function createCounter(n) {
-
+return n
 }
 
+function counter(createCounter) {
+ return createCounter + 1
+}
+module.exports = counter;
 module.exports = createCounter;
+// npm run test part-1
+// npm run new-task
