2c1f5323e70572fe848da20c7d1420ef098edf78
zmkt <zmkt@mail.ru>
2024-06-21 18:17:36 +0300

    done

--- part-1/index.js
+++ part-1/index.js
@@ -1,5 +1,13 @@
-function createCounter(n) {
 
+function createCounter(n) {
+	let res = 0
+	function counter(){
+		return n + res
+	}
+	res 
+	return res
 }
+
+console.log(createCounter(1))
 
 module.exports = createCounter;
